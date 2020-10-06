cf231a746b7651a60bc8775f8a798e5cd24c3063
root <root@LAPTOP-RR93DGL7.localdomain>
2020-10-05 23:25:43 -0500

    deployment

---
+++ precache-manifest.1f7e557d1ad74a2a6d5ea90d7f2cefa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "759ce86c68daf5636a679da78488893e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4aabcbc8c7bdbff4147",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7f7506d485d96d436dd5",
+    "url": "/static/js/2.4cbe87e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4cbe87e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4aabcbc8c7bdbff4147",
+    "url": "/static/js/main.808ffeb6.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
